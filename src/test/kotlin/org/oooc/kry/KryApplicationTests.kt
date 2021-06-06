e8ad4ec279faf000b0cc5d5ff402422866d232da
jerry901 <pkhu2000@gmail.com>
2021-06-06 17:34:02 +0900

    [ADD] datasource

--- src/test/kotlin/org/oooc/kry/KryApplicationTests.kt
+++ src/test/kotlin/org/oooc/kry/KryApplicationTests.kt
@@ -5,9 +5,12 @@ import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class KryApplicationTests {
-
     @Test
     fun contextLoads() {
+        println("###SpringBootTest Run Success###")
+        println("###SpringBootTest Run Success###")
+        println("###SpringBootTest Run Success###")
+        println("###SpringBootTest Run Success###")
+        println("###SpringBootTest Run Success###")
     }
-
 }
