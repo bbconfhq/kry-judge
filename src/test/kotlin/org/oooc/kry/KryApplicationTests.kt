7187acaa5ce9a8932563432c6027b340c79158c4
dsyun96 <dsyun96@gmail.com>
2021-06-06 15:52:22 +0900

    init setting

---
+++ src/test/kotlin/org/oooc/kry/KryApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.oooc.kry
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KryApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
