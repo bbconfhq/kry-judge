6b999a411979583248110b4a2c912f02c9db38aa
jerry901 <pkhu2000@gmail.com>
2021-07-29 19:42:38 +0900

    [ADD] BaseEntity

---
+++ src/main/kotlin/org/oooc/kry/global/entity/BaseEntity.kt
@@ -0,0 +1,7 @@
+package org.oooc.kry.global.entity
+
+import java.io.Serializable
+import javax.persistence.MappedSuperclass
+
+@MappedSuperclass
+open class BaseEntity: Serializable{}
---
+++ src/main/kotlin/org/oooc/kry/global/package-info.java
@@ -0,0 +1,1 @@
+package org.oooc.kry.global;
