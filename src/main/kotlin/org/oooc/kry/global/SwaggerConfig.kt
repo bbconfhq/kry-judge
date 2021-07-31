61825a7db5593ad12c3fb21e089fcc15860183f0
jerry901 <pkhu2000@gmail.com>
2021-07-31 16:40:31 +0900

    [UPDATE] Swagger config

--- src/main/kotlin/org/oooc/kry/global/SwaggerConfig.kt
+++ src/main/kotlin/org/oooc/kry/global/SwaggerConfig.kt
@@ -1,3 +1,5 @@
+package org.oooc.kry.global
+
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import springfox.documentation.builders.PathSelectors
@@ -5,7 +7,6 @@ import springfox.documentation.builders.RequestHandlerSelectors
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
-import java.util.function.Predicate
 
 
 @Configuration
