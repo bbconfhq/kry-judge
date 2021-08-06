fc6501e9965094f374d1606a44dde914ae7551b9
dsyun96 <dsyun96@gmail.com>
2021-08-06 18:51:34 +0900

    num 파라미터 추가

--- src/main/kotlin/org/oooc/kry/problem/controller/ProblemSetController.kt
+++ src/main/kotlin/org/oooc/kry/problem/controller/ProblemSetController.kt
@@ -3,20 +3,26 @@ package org.oooc.kry.problem.controller
 import org.oooc.kry.problem.entity.Problem
 import org.oooc.kry.problem.service.ProblemService
 import org.springframework.data.domain.PageRequest
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/problemset")
 class ProblemSetController(
     private val problemService: ProblemService
 ) {
-    val PROBLEM_NUMBER_PER_PAGE = 10
+    companion object {
+        const val MAX_PROBLEM_NUMBER_PER_PAGE = 10
+    }
 
     @GetMapping("", "/{page}")
-    fun getProblemList(@PathVariable(value = "page", required = false) page: Int?): List<Problem> {
-        return problemService.getProblemList(PageRequest.of(page ?: 0, PROBLEM_NUMBER_PER_PAGE))
+    fun getProblemList(
+        @PathVariable(value = "page", required = false) page: Int?,
+        @RequestParam(defaultValue = "$MAX_PROBLEM_NUMBER_PER_PAGE") num: Int
+    ): List<Problem> {
+        if (num > MAX_PROBLEM_NUMBER_PER_PAGE || num < 1) {
+            throw IllegalArgumentException("Parameter(num) must be 1 <= num <= $MAX_PROBLEM_NUMBER_PER_PAGE")
+        }
+
+        return problemService.getProblemList(PageRequest.of(page ?: 0, num))
     }
 }
