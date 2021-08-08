dfd87e9c27d8f43883c4c2c59fe403d7bbcaf9c1
berry2971 <berry2971@hanmail.net>
2021-08-08 11:08:32 +0900

    id classes for multiple PK have been integrated to file which depends on id classes

--- src/main/kotlin/org/oooc/kry/board/domain/entity/ArticleVote.kt
+++ src/main/kotlin/org/oooc/kry/board/domain/entity/ArticleVote.kt
@@ -3,18 +3,43 @@ package org.oooc.kry.board.domain.entity
 import java.io.Serializable
 import javax.persistence.*
 
+class ArticleVoteId (
+    val article: Article = Article(),
+    // val user: User = User(),
+    val updown: Byte = 0
+): Serializable {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ArticleVoteId
+
+        if (article != other.article) return false
+        if (updown != other.updown) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = article.hashCode()
+        result = 31 * result + updown.hashCode()
+        return result
+    }
+}
+
 @Entity
-//@IdClass(ArticleVoteId::class)
+@IdClass(ArticleVoteId::class)
 class ArticleVote (
 
     @Id
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn
+    @JoinColumn(name = "article_id")
     val article: Article = Article(),
 
 //    TODO: Mapping to User
 //    @Id
 //    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id")
 //    val user: User = User(),
 
     @Id
--- src/main/kotlin/org/oooc/kry/board/domain/entity/CommentVote.kt
+++ src/main/kotlin/org/oooc/kry/board/domain/entity/CommentVote.kt
@@ -3,17 +3,43 @@ package org.oooc.kry.board.domain.entity
 import java.io.Serializable
 import javax.persistence.*
 
+class CommentVoteId (
+    val comment: Comment = Comment(),
+    // val user: User = User(),
+    val updown: Byte = 0
+): Serializable {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as CommentVoteId
+
+        if (comment != other.comment) return false
+        if (updown != other.updown) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = comment.hashCode()
+        result = 31 * result + updown.hashCode()
+        return result
+    }
+}
+
 @Entity
-//@IdClass(CommentVoteId::class)
+@IdClass(CommentVoteId::class)
 class CommentVote (
 
     @Id
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id")
     val comment: Comment = Comment(),
 
 //    TODO: Mapping to User
-//    @ManyToOne
 //    @Id
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id")
 //    val user: User = User(),
 
     @Id
