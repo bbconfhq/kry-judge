ea3ea5b9bf68a26715df7d55c7c339f33718eea7
dsyun96 <dsyun96@gmail.com>
2021-08-12 00:01:26 +0900

    init sql문 수정

--- db/initdb.d/init.sql
+++ db/initdb.d/init.sql
@@ -1,23 +1,26 @@
--- This script has possibility of NOT create anything!
-
-CREATE DATABASE IF NOT EXISTS kry;
+CREATE DATABASE IF NOT EXISTS kry
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
 USE kry;
 
 CREATE TABLE problem (
     id BIGINT NOT NULL AUTO_INCREMENT,
-    accept_count INTEGER NOT NULL,
-    content TEXT NOT NULL,
-    created DATETIME(6) NOT NULL,
-    input TEXT NOT NULL,
-    memory_limit INTEGER NOT NULL,
-    modified DATETIME(6) NOT NULL,
-    note TEXT NOT NULL,
-    output TEXT NOT NULL,
-    submit_count INTEGER NOT NULL,
-    time_limit DECIMAL(7, 5) NOT NULL,
-    title VARCHAR(191) NOT NULL,
-    PRIMARY KEY (id)
-) engine=InnoDB;
+    title VARCHAR(191) NOT NULL DEFAULT "",
+    content TEXT NOT NULL DEFAULT "",
+    created DATETIME(6) NOT NULL DEFAULT NOW(),
+    modified DATETIME(6) NOT NULL DEFAULT NOW(),
+    input TEXT NOT NULL DEFAULT "",
+    output TEXT NOT NULL DEFAULT "",
+    note TEXT NOT NULL DEFAULT "",
+    time_limit DECIMAL(7, 5) NOT NULL DEFAULT 1.0,
+    memory_limit INTEGER NOT NULL DEFAULT 128,
+    submit_count INTEGER NOT NULL DEFAULT 0,
+    accept_count INTEGER NOT NULL DEFAULT 0,
+    PRIMARY KEY (id),
+    UNIQUE KEY (title)
+) ENGINE = InnoDB;
+
+ALTER TABLE problem AUTO_INCREMENT = 1000;
 
 CREATE TABLE test_case (
     id BIGINT NOT NULL AUTO_INCREMENT,
@@ -25,16 +28,17 @@ CREATE TABLE test_case (
     output TEXT NOT NULL,
     problem_id BIGINT,
     PRIMARY KEY (id)
-) engine=InnoDB;
+) ENGINE = InnoDB;
 
 CREATE TABLE tag (
     id BIGINT NOT NULL AUTO_INCREMENT,
     name VARCHAR(191) NOT NULL,
     PRIMARY KEY (id)
-) engine=InnoDB;
+) ENGINE = InnoDB;
 
-INSERT INTO problem VALUES(1, 10, "두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.", "2020-03-13 11:04:39", "첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)", 128, "2020-03-13 11:04:39", "", "첫째 줄에 A+B를 출력한다.", 55, 1.0, "A+B");
-INSERT INTO problem VALUES(2, 45, "두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.", "2020-03-13 11:04:39", "첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)", 128, "2020-03-13 11:04:39", "", "첫째 줄에 A-B를 출력한다.", 743, 1.0, "A-B");
-INSERT INTO problem VALUES(3, 2, "조규현과 백승환은 터렛에 근무하는 직원이다. 하지만 워낙 존재감이 없어서 인구수는 차지하지 않는다. 다음은 조규현과 백승환의 사진이다. 이석원은 조규현과 백승환에게 상대편 마린(류재명)의 위치를 계산하라는 명령을 내렸다. 조규현과 백승환은 각각 자신의 터렛 위치에서 현재 적까지의 거리를 계산했다. 조규현의 좌표 (x1, y1)와 백승환의 좌표 (x2, y2)가 주어지고, 조규현이 계산한 류재명과의 거리 r1과 백승환이 계산한 류재명과의 거리 r2가 주어졌을 때, 류재명이 있을 수 있는 좌표의 수를 출력하는 프로그램을 작성하시오.", "2021-01-22 03:24:31", "첫째 줄에 테스트 케이스의 개수 T가 주어진다. 각 테스트 케이스는 다음과 같이 이루어져 있다. 한 줄에 x1, y1, r1, x2, y2, r2가 주어진다. x1, y1, x2, y2는 -10,000보다 크거나 같고, 10,000보다 작거나 같은 정수이고, r1, r2는 10,000보다 작거나 같은 자연수이다.", 128, "2021-01-22 03:26:04", "", "각 테스트 케이스마다 류재명이 있을 수 있는 위치의 수를 출력한다. 만약 류재명이 있을 수 있는 위치의 개수가 무한대일 경우에는 -1을 출력한다.", 5, 2.0, "터렛");
-INSERT INTO problem VALUES(4, 79200, "두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.", "2021-07-11 22:22:22", "첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)", 128, "2021-07-11 22:22:22", "", "첫째 줄에 A/B를 출력한다.", 287310, 1.0, "A/B");
-INSERT INTO problem VALUES(5, 2606, "P[0], P[1], ...., P[N-1]은 0부터 N-1까지(포함)의 수를 한 번씩 포함하고 있는 수열이다. 수열 P를 길이가 N인 배열 A에 적용하면 길이가 N인 배열 B가 된다. 적용하는 방법은 B[P[i]] = A[i]이다. 배열 A가 주어졌을 때, 수열 P를 적용한 결과가 비내림차순이 되는 수열을 찾는 프로그램을 작성하시오. 비내림차순이란, 각각의 원소가 바로 앞에 있는 원소보다 크거나 같을 경우를 말한다. 만약 그러한 수열이 여러개라면 사전순으로 앞서는 것을 출력한다.", "2010-03-13 11:04:39", "첫째 줄에 배열 A의 크기 N이 주어진다. 둘째 줄에는 배열 A의 원소가 0번부터 차례대로 주어진다. N은 50보다 작거나 같은 자연수이고, 배열의 원소는 1,000보다 작거나 같은 자연수이다.", 128, "2010-03-13 11:04:39", "", "첫째 줄에 비내림차순으로 만드는 수열 P를 출력한다.", 7050, 1.0, "수열 정렬");
+INSERT INTO problem (title, content, input, output)
+    VALUES ("A+B", "두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.", "첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)", "첫째 줄에 A+B를 출력한다."),
+           ("A-B", "두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.", "첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)", "첫째 줄에 A-B를 출력한다."),
+           ("감소하는 수", "음이 아닌 정수 X의 자릿수가 가장 큰 자릿수부터 작은 자릿수까지 감소한다면, 그 수를 감소하는 수라고 한다. 예를 들어, 321과 950은 감소하는 수지만, 322와 958은 아니다. N번째 감소하는 수를 출력하는 프로그램을 작성하시오. 0은 0번째 감소하는 수이고, 1은 1번째 감소하는 수이다. 만약 N번째 감소하는 수가 없다면 -1을 출력한다.", "첫째 줄에 N이 주어진다. N은 1,000,000보다 작거나 같은 자연수 또는 0이다.", "첫째 줄에 N번째 감소하는 수를 출력한다."),
+           ("소트", "크기가 N인 배열 A가 있다. 배열에 있는 모든 수는 서로 다르다. 이 배열을 소트할 때, 연속된 두 개의 원소만 교환할 수 있다. 그리고, 교환은 많아봐야 S번 할 수 있다. 이때, 소트한 결과가 사전순으로 가장 뒷서는 것을 출력한다.", "첫째 줄에 N이 주어진다. N은 50보다 작거나 같은 자연수이다. 둘째 줄에는 각 원소가 차례대로 주어진다. 이 값은 1000000보다 작거나 같은 자연수이다. 마지막 줄에는 S가 주어진다. S는 1000000보다 작거나 같은 음이 아닌 정수이다.", "첫째 줄에 문제의 정답을 출력한다."),
+           ("동전 1", "n가지 종류의 동전이 있다. 각각의 동전이 나타내는 가치는 다르다. 이 동전을 적당히 사용해서, 그 가치의 합이 k원이 되도록 하고 싶다. 그 경우의 수를 구하시오. 각각의 동전은 몇 개라도 사용할 수 있다. 사용한 동전의 구성이 같은데, 순서만 다른 것은 같은 경우이다.", "첫째 줄에 n, k가 주어진다. (1 ≤ n ≤ 100, 1 ≤ k ≤ 10,000) 다음 n개의 줄에는 각각의 동전의 가치가 주어진다. 동전의 가치는 100,000보다 작거나 같은 자연수이다.", "첫째 줄에 경우의 수를 출력한다. 경우의 수는 2^31보다 작다.");
