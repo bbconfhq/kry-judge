70f2594169aeab870b8744f3d3bf8b1db58326bb
dsyun96 <dsyun96@gmail.com>
2021-08-15 16:24:52 +0900

    테스트 코드에 한글 테스트 추가

--- src/test/kotlin/org/oooc/kry/problem/entity/ProblemTest.kt
+++ src/test/kotlin/org/oooc/kry/problem/entity/ProblemTest.kt
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.oooc.kry.problem.repository.ProblemRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
@@ -15,5 +16,6 @@ class ProblemTest {
     fun getProblemByTitle() {
         val problem = problemRepository.findByTitle("A+B")
         assertThat(problem?.title).isEqualTo("A+B")
+        assertThat(problem?.content).isEqualTo("두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.")
     }
 }
