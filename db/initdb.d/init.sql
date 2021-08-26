3db88653fe27b2bfe256ef30ed69f16cd178c89c
YunGoon <dsyun96@gmail.com>
2021-08-26 21:20:55 +0900

    문제 업데이트 시 modified 값 갱신 (#47)
    * is_example 컬럼명 수정
    
    * updateProblem modified 갱신
    
    * testcase의 fk인 problem_id null 비허용

--- db/initdb.d/init.sql
+++ db/initdb.d/init.sql
@@ -24,10 +24,10 @@ ALTER TABLE problem AUTO_INCREMENT = 1000;
 
 CREATE TABLE testcase (
     id BIGINT NOT NULL AUTO_INCREMENT,
-    is_example BOOLEAN NOT NULL DEFAULT false,
+    visible BOOLEAN NOT NULL DEFAULT false,
     input TEXT NOT NULL DEFAULT "",
     output TEXT NOT NULL DEFAULT "",
-    problem_id BIGINT,
+    problem_id BIGINT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (problem_id) REFERENCES problem (id)
 ) ENGINE = InnoDB;
@@ -53,7 +53,7 @@ INSERT INTO problem (title, content, input, output)
            ("소트", "크기가 N인 배열 A가 있다. 배열에 있는 모든 수는 서로 다르다. 이 배열을 소트할 때, 연속된 두 개의 원소만 교환할 수 있다. 그리고, 교환은 많아봐야 S번 할 수 있다. 이때, 소트한 결과가 사전순으로 가장 뒷서는 것을 출력한다.", "첫째 줄에 N이 주어진다. N은 50보다 작거나 같은 자연수이다. 둘째 줄에는 각 원소가 차례대로 주어진다. 이 값은 1000000보다 작거나 같은 자연수이다. 마지막 줄에는 S가 주어진다. S는 1000000보다 작거나 같은 음이 아닌 정수이다.", "첫째 줄에 문제의 정답을 출력한다."),
            ("동전 1", "n가지 종류의 동전이 있다. 각각의 동전이 나타내는 가치는 다르다. 이 동전을 적당히 사용해서, 그 가치의 합이 k원이 되도록 하고 싶다. 그 경우의 수를 구하시오. 각각의 동전은 몇 개라도 사용할 수 있다. 사용한 동전의 구성이 같은데, 순서만 다른 것은 같은 경우이다.", "첫째 줄에 n, k가 주어진다. (1 ≤ n ≤ 100, 1 ≤ k ≤ 10,000) 다음 n개의 줄에는 각각의 동전의 가치가 주어진다. 동전의 가치는 100,000보다 작거나 같은 자연수이다.", "첫째 줄에 경우의 수를 출력한다. 경우의 수는 2^31보다 작다.");
 
-INSERT INTO testcase (problem_id, is_example, input, output)
+INSERT INTO testcase (problem_id, visible, input, output)
     VALUES (1000, true, "1 2", "3"),
            (1000, false, "3 8", "11"),
            (1000, false, "9 9", "18");
