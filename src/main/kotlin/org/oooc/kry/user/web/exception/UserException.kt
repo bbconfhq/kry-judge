90a7dcdec40897ce4983b549b7160f0990e4e077
jerry901 <pkhu2000@gmail.com>
2021-09-18 12:29:35 +0900

    refactor

--- src/main/kotlin/org/oooc/kry/user/web/exception/UserException.kt
+++
@@ -1,4 +0,0 @@
-package org.oooc.kry.user.web.exception
-
-class UserNotFoundException(message: String = "유저를 찾을 수 없습니다.") : RuntimeException(message)
-class WrongPasswordException(message: String = "비밀번호가 일치하지 않습니다.") : RuntimeException(message)
