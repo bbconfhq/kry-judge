e0ecc9536950acf29fa287eaab46863341de5dc0
jerry901 <pkhu2000@gmail.com>
2021-09-18 12:36:40 +0900

    add service, controller

--- src/main/kotlin/org/oooc/kry/auth/web/controller/AuthController.kt
+++ src/main/kotlin/org/oooc/kry/auth/web/controller/AuthController.kt
@@ -1,9 +1,6 @@
 package org.oooc.kry.auth.web.controller
 
-import org.oooc.kry.auth.domain.dto.PermgroupPostRequestDTO
-import org.oooc.kry.auth.domain.dto.PermgroupPostResponseDTO
-import org.oooc.kry.auth.domain.dto.PermgroupGetResponseDTO
-import org.oooc.kry.auth.domain.dto.PermissionGetResponseDTO
+import org.oooc.kry.auth.domain.dto.*
 import org.oooc.kry.auth.web.service.AuthService
 import org.oooc.kry.global.dto.APIResponse
 import org.springframework.web.bind.annotation.*
@@ -14,16 +11,9 @@ import javax.validation.Valid
 class AuthController (
     private val authService: AuthService
 ) {
-    @GetMapping("/permgroup/list")
-    fun getPermgroupList(): APIResponse<List<PermgroupGetResponseDTO>> {
-        return APIResponse(authService.getPermgroupList())
-    }
-    @GetMapping("permission/list")
-    fun getPermissionList(): APIResponse<List<PermissionGetResponseDTO>> {
-        return APIResponse(authService.getPermissionList())
-    }
-    @PostMapping("/permgroup")
-    fun addPermgroup(@RequestBody @Valid permgroupPostRequestDTO: PermgroupPostRequestDTO): APIResponse<PermgroupPostResponseDTO> {
-        return APIResponse(authService.addPermgroup(permgroupPostRequestDTO))
-    }
+//    @GetMapping("/permgroup")
+//    fun getUserPrivate(): APIResponse<PermgroupDTO> {
+//
+//        return
+//    }
 }
--- src/main/kotlin/org/oooc/kry/auth/web/service/AuthService.kt
+++ src/main/kotlin/org/oooc/kry/auth/web/service/AuthService.kt
@@ -2,7 +2,10 @@ package org.oooc.kry.auth.web.service
 
 import org.oooc.kry.auth.domain.dto.*
 import org.oooc.kry.auth.domain.entity.Permgroup
-import org.oooc.kry.auth.domain.repository.*
+import org.oooc.kry.auth.domain.entity.Permission
+import org.oooc.kry.auth.domain.exception.PermgroupNotFoundException
+import org.oooc.kry.auth.web.repository.*
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
@@ -13,23 +16,38 @@ class AuthService (
     private val userPermgroupRepository: UserPermgroupRepository,
     private val userPermissionRepository: UserPermissionRepository
 ) {
-    fun getPermgroupList(): List<PermgroupGetResponseDTO> {
-        return permgroupRepository.findAllResponseDTOProjectionBy()
+    fun getPermgroupList(): List<PermgroupDTO> {
+        val permgroups = permgroupRepository.findAll()
+        val permgroupDTOs = permgroups.map { permgroup ->
+            PermgroupDTO(permgroup.id, permgroup.name)
+        }
+
+        return permgroupDTOs
     }
 
-    fun addPermgroup(permgroupPostRequestDTO: PermgroupPostRequestDTO): PermgroupPostResponseDTO {
-        val permgroup = permgroupRepository.save(
-            Permgroup(
-                name = permgroupPostRequestDTO.name
-            )
-        )
-
-        return PermgroupPostResponseDTO(
-            permgroup.name
-        )
+    fun addPermgroup(name: String): PermgroupDTO {
+        val permgroup = permgroupRepository.save(Permgroup(name = name))
+        return PermgroupDTO(id = permgroup.id, name = permgroup.name)
     }
 
-    fun getPermissionList(): List<PermissionGetResponseDTO> {
-        return permissionRepository.findAllGetResponseDTOProjectionBy()
+    fun updatePermgroup(permgroupUpdateDTO: PermgroupUpdateDTO) {
+        val newPermgroup = permgroupRepository.findByIdOrNull(permgroupUpdateDTO.id) ?: throw PermgroupNotFoundException()
+        newPermgroup.apply {
+            name = permgroupUpdateDTO.name
+        }
+    }
+
+    fun getPermissionList(): List<PermissionDTO> {
+        val permissions = permissionRepository.findAll()
+        val permissionDTOs = permissions.map { permission->
+            PermissionDTO(permission.id, permission.name)
+        }
+
+        return permissionDTOs
+    }
+
+    fun addPermission(name: String): PermissionDTO {
+        val permission = permissionRepository.save(Permission(name = name))
+        return PermissionDTO(id = permission.id, name = permission.name)
     }
 }
